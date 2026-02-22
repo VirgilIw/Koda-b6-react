342a6f5c011adb39743ec82b12715b81d2097565
Virgil I Ambar <imanuelambar11@gmail.com>
2026-02-22 23:36:17 +0700

    feat: add admin dashboard page

--- src/pages/AdminDashboard.jsx
+++ src/pages/AdminDashboard.jsx
@@ -1,7 +1,12 @@
-import React from 'react'
+import React from "react";
+import AdminDashboardMenu from "../components/admin/AdminDashboardMenu";
+import ProductChart from "../components/admin/ProductChart";
 
 export default function AdminDashboard() {
   return (
-    <div>AdminDashboard</div>
-  )
+    <main className="px-5 md:grid md:grid-cols-[300px_670px] lg:grid-cols-[20%_80%] mt-24">
+      <AdminDashboardMenu />
+      <ProductChart />
+    </main>
+  );
 }
