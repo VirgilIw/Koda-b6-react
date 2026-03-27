5fc456dc228e4f67e1a978e3ef6c896cba74437c
Virgil I Ambar <imanuelambar11@gmail.com>
2026-03-27 16:11:10 +0700

    feat: add whitelist and new reducer to store redux

--- src/redux/store.js
+++ src/redux/store.js
@@ -9,17 +9,26 @@ import {
   REHYDRATE,
 } from "redux-persist";
 import storage from "redux-persist/lib/storage";
+
 import productReducer from "./slice/product.slice";
 import orderReducer from "./slice/order.slice";
+import authReducer from "./slice/auth.slice";
+import metaReducer from "./slice/meta.slice";
+import cartReducer from "./slice/cart.slice";
 
 const persistConfig = {
   key: "data",
   storage,
+  whitelist: ["auth"],
 };
 
+
 const persistedReducer = persistCombineReducers(persistConfig, {
+  auth: authReducer,
   product: productReducer,
   order: orderReducer,
+  meta: metaReducer, 
+  cart: cartReducer,
 });
 
 const store = configureStore({
