c3568ad525b51c8d23f885124d45d0bfabad86a2
Virgil I Ambar <imanuelambar11@gmail.com>
2026-03-30 00:47:21 +0700

    feat: add admin dashboard menu component

--- src/layout/AdminLayout.jsx
+++ src/layout/AdminLayout.jsx
@@ -1,13 +1,18 @@
 import React from "react";
 import Navbar from "../components/ui/Navbar";
 import { Outlet } from "react-router";
+import AdminDashboardMenu from "../components/admin/AdminDashboardMenu";
 
 export default function AdminLayout() {
   return (
     <div>
       <Navbar />
-      <main className={`min-h-screen`}>
-        <Outlet />
+      <main className="mt-15 flex min-h-screen">
+          <AdminDashboardMenu />
+
+        <div className="w-screen">
+          <Outlet />
+        </div>
       </main>
     </div>
   );
