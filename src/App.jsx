37c43547dd55bdea1fdc156673490793c0a9f452
Virgil I Ambar <imanuelambar11@gmail.com>
2026-04-14 12:01:31 +0700

    feat: add new privateroute

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import store, { persistedStore } from "./redux/store";
 import { PersistGate } from "redux-persist/integration/react";
 import { Provider } from "react-redux";
 import RegisterProvider from "./contexts/register/RegisterProvider";
+
 import Home from "./pages/Home";
 import Product from "./pages/Product";
 import MainLayout from "./layout/MainLayout";
@@ -14,81 +15,57 @@ import CheckoutProduct from "./pages/CheckoutProduct";
 import HistoryOrder from "./pages/HistoryOrder";
 import OrderDetail from "./pages/OrderDetail";
 import Profile from "./pages/Profile";
+
 import AdminDashboard from "./pages/AdminDashboard";
 import AdminLayout from "./layout/AdminLayout";
+import AdminProductList from "./pages/AdminProductList";
+
 import ForgotPassword from "./pages/ForgotPassword";
 import ResetPassword from "./pages/ResetPassword";
-import AdminProductList from "./pages/AdminProductList";
+import PrivateRoute from "./routes/PrivateRoutes";
+
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <MainLayout />,
     children: [
+      { index: true, element: <Home /> },
+      { path: "product", element: <Product /> },
+      { path: "product/:id", element: <DetailProduct /> },
+
       {
-        index: true,
-        element: <Home />,
+        element: <PrivateRoute />,
+        children: [
+          { path: "checkout-product/:id", element: <CheckoutProduct /> },
+          { path: "history-order", element: <HistoryOrder /> },
+          { path: "detail-order/:id", element: <OrderDetail /> },
+          { path: "profile", element: <Profile /> },
+        ],
       },
-      {
-        path: "product",
-        element: <Product />,
-      },
-      {
-        path: "product/:id",
-        element: <DetailProduct />,
-      },
-      {
-        path: "checkout-product/:id",
-        element: <CheckoutProduct />,
-      },
-      {
-        path: "history-order",
-        element: <HistoryOrder />,
-      },
-      {
-        path: "detail-order/:id",
-        element: <OrderDetail />,
-      },
-      {
-        path: "profile",
-        element: <Profile />,
-      },
-      {
-        path: "*",
-        element: <Error />,
-      },
+
+      { path: "*", element: <Error /> },
     ],
   },
+
   {
     path: "/dashboard/admin",
-    element: <AdminLayout />,
+    element: <PrivateRoute requireAdmin={true} />,
     children: [
       {
-        index: true,
-        element: <AdminDashboard />,
-      },
-      {
-        path: "product-list",
-        element: <AdminProductList />,
+        element: <AdminLayout />,
+        children: [
+          { index: true, element: <AdminDashboard /> },
+          { path: "product-list", element: <AdminProductList /> },
+        ],
       },
     ],
   },
-  {
-    path: "/login",
-    element: <Login />,
-  },
-  {
-    path: "/register",
-    element: <Register />,
-  },
-  {
-    path: "/forgot-password",
-    element: <ForgotPassword />,
-  },
-  {
-    path: "/reset-password",
-    element: <ResetPassword />,
-  },
+
+  { path: "/login", element: <Login /> },
+  { path: "/register", element: <Register /> },
+  { path: "/forgot-password", element: <ForgotPassword /> },
+  { path: "/reset-password", element: <ResetPassword /> },
 ]);
 
 const App = () => {
